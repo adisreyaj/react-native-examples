527aeed46c9b4848c04dc50fcf271069a6eff7c3
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-06-26 00:20:28 +0530

    💫  Adding animations

--- screens/Storage/StorageHome/StorageMyDrives.js
+++ screens/Storage/StorageHome/StorageMyDrives.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Animated,
+  Dimensions,
+  TouchableOpacity,
+  Easing,
+  FlatList,
+} from 'react-native';
 import { human } from 'react-native-typography';
 import { storageTheme, driveData } from '../config/config';
 import { LinearGradient } from 'expo-linear-gradient';
 const StorageMyDrives = () => {
+  const scroller = useRef(undefined);
+  const scrollX = new Animated.Value(0);
+
+  const [scrollPosition, setScrollPosition] = useState(undefined);
+  const animation = Animated.timing(scrollX, {
+    toValue: 120,
+    duration: 200,
+  });
+  const [selected, setSelected] = useState(1);
+
+  useEffect(() => {
+    if (scroller.current && selected)
+      scroller.current.scrollToIndex({
+        index: selected - 1,
+        animted: true,
+        viewOffset: 90,
+      });
+  }, [selected]);
+
+  // useEffect(() => {
+  //   const index = Math.round(scrollPosition / 290);
+  //   if (scrollPosition < 100) setSelected(1);
+  //   else setSelected(index >= driveData.length ? driveData.length : index + 1);
+  // }, [scrollPosition]);
+
+  const changeSelection = (id) => setSelected(id);
   return (
     <View style={styles.container}>
       <View style={{ marginHorizontal: 24 }}>
@@ -13,16 +50,150 @@ const StorageMyDrives = () => {
           My Storage
         </Text>
       </View>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ marginTop: 24 }}>
-        {driveData.map((item) => (
-          <StorageMyDrivesCard key={item.id} {...item} />
-        ))}
-      </ScrollView>
+      <FlatList
+        style={{ marginTop: 24 }}
+        horizontal
+        initialScrollIndex={selected}
+        onScrollToIndexFailed={(info) => {
+          const wait = new Promise((resolve) => setTimeout(resolve, 500));
+          wait.then(() => {
+            flatList.current?.scrollToIndex({ index: 1, animated: true });
+          });
+        }}
+        onScroll={(e) => setScrollPosition(e.nativeEvent.contentOffset.x)}
+        data={driveData}
+        keyExtractor={(item) => item.id}
+        showsHorizontalScrollIndicator={false}
+        ref={scroller}
+        renderItem={({ item, i }) => (
+          <StorageMyDrivesCard
+            key={item.id}
+            {...item}
+            scrollX={scrollX}
+            selected={selected === item.id}
+            index={i}
+            select={changeSelection}
+          />
+        )}
+      />
     </View>
   );
 };
 
 export default StorageMyDrives;
+
+const StorageMyDrivesCard = ({
+  id,
+  name,
+  logo,
+  consumed,
+  total,
+  selected = false,
+  scrollX,
+  select,
+}) => {
+  const transY = useRef(new Animated.Value(0)).current;
+  const animation = Animated.timing(transY, {
+    toValue: selected ? 0 : 12,
+    duration: 300,
+    easing: Easing.bezier(0.17, 0.67, 0.82, 0.98),
+  });
+
+  useEffect(() => {
+    animation.start();
+  }, [selected]);
+  return (
+    <TouchableOpacity onPress={() => select(id)} activeOpacity={0.9}>
+      <Animated.View style={{}}>
+        <Animated.View
+          style={{
+            paddingTop: 16,
+            marginBottom: 48,
+            marginHorizontal: 16,
+            transform: [
+              {
+                translateY: transY,
+              },
+            ],
+          }}
+        >
+          <View
+            style={[
+              styles.card,
+              {
+                backgroundColor: selected ? storageTheme.accent : storageTheme.bg,
+                borderRadius: 8,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+              },
+            ]}
+          >
+            <View style={{ position: 'absolute', top: 24, right: 24 }}>
+              <Text
+                style={[
+                  human.headline,
+                  { fontWeight: '700', color: selected ? '#fff' : storageTheme.textDark },
+                ]}
+              >
+                {consumed}/{total}GB
+              </Text>
+            </View>
+
+            <View style={{ paddingBottom: 20, paddingHorizontal: 16, width: '100%' }}>
+              <Text
+                style={[
+                  human.title2,
+                  {
+                    fontWeight: '700',
+                    marginBottom: 16,
+                    color: selected ? '#fff' : storageTheme.textDark,
+                  },
+                ]}
+              >
+                {name}
+              </Text>
+              <View
+                style={{
+                  width: '100%',
+                  backgroundColor: '#fff',
+                  height: 18,
+                  borderRadius: 20,
+                  justifyContent: 'center',
+                  paddingHorizontal: 5,
+                }}
+              >
+                <View
+                  style={{
+                    width: `${(consumed / total) * 100}%`,
+                    backgroundColor: storageTheme.accent,
+                    height: 8,
+                    borderRadius: 20,
+                  }}
+                ></View>
+              </View>
+            </View>
+          </View>
+          <View
+            style={[
+              styles.cardFolderHeaderExtra,
+              { backgroundColor: selected ? storageTheme.accent : storageTheme.bg },
+            ]}
+          ></View>
+          <View
+            style={[
+              styles.cardFolderHeader,
+              { backgroundColor: selected ? storageTheme.accent : storageTheme.bg },
+            ]}
+          >
+            <View style={styles.cardImageContainer}>
+              <Image resizeMode="contain" style={styles.cardImage} source={logo} />
+            </View>
+          </View>
+        </Animated.View>
+      </Animated.View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -74,96 +245,3 @@ const styles = StyleSheet.create({
     zIndex: 8,
   },
 });
-
-const StorageMyDrivesCard = ({ name, logo, consumed, total, selected = false }) => {
-  return (
-    <View>
-      <View
-        style={{
-          paddingTop: 16,
-          marginBottom: 48,
-          marginHorizontal: 16,
-
-          transform: [
-            {
-              translateY: selected ? 0 : 12,
-            },
-          ],
-        }}
-      >
-        <View
-          style={[
-            styles.card,
-            {
-              backgroundColor: selected ? storageTheme.accent : storageTheme.bg,
-              borderRadius: 8,
-              flexDirection: 'row',
-              alignItems: 'flex-end',
-            },
-          ]}
-        >
-          <View style={{ position: 'absolute', top: 24, right: 24 }}>
-            <Text
-              style={[
-                human.headline,
-                { fontWeight: '700', color: selected ? '#fff' : storageTheme.textDark },
-              ]}
-            >
-              {consumed}/{total}GB
-            </Text>
-          </View>
-
-          <View style={{ paddingBottom: 20, paddingHorizontal: 16, width: '100%' }}>
-            <Text
-              style={[
-                human.title2,
-                {
-                  fontWeight: '700',
-                  marginBottom: 16,
-                  color: selected ? '#fff' : storageTheme.textDark,
-                },
-              ]}
-            >
-              {name}
-            </Text>
-            <View
-              style={{
-                width: '100%',
-                backgroundColor: '#fff',
-                height: 18,
-                borderRadius: 20,
-                justifyContent: 'center',
-                paddingHorizontal: 5,
-              }}
-            >
-              <View
-                style={{
-                  width: `${(consumed / total) * 100}%`,
-                  backgroundColor: storageTheme.accent,
-                  height: 8,
-                  borderRadius: 20,
-                }}
-              ></View>
-            </View>
-          </View>
-        </View>
-        <View
-          style={[
-            styles.cardFolderHeaderExtra,
-            { backgroundColor: selected ? storageTheme.accent : storageTheme.bg },
-          ]}
-        ></View>
-        <View
-          style={[
-            styles.cardFolderHeader,
-            { backgroundColor: selected ? storageTheme.accent : storageTheme.bg },
-          ]}
-        >
-          <View style={styles.cardImageContainer}>
-            <Image resizeMode="contain" style={styles.cardImage} source={logo} />
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
--- screens/Storage/config/config.js
+++ screens/Storage/config/config.js
@@ -23,4 +23,20 @@ export const driveData = [
     total: 100,
     selected: false,
   },
+  {
+    id: 3,
+    name: 'OneDrive',
+    logo: require('../../../assets/storage/onedrive.png'),
+    consumed: 2,
+    total: 10,
+    selected: false,
+  },
+  {
+    id: 4,
+    name: 'Box',
+    logo: require('../../../assets/storage/box.png'),
+    consumed: 1,
+    total: 5,
+    selected: false,
+  },
 ];
